922e28c6fb4685ba32906d8497f3e654cfa198c9
PranavTadvi <p.d.tadvi8825@gmail.com>
2025-02-25 13:03:29 +0530

    Created educator dashboard page and listed enrolled students and also created my courses page & listed all courses.

--- src/components/educator/Footer.jsx
+++ src/components/educator/Footer.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Edu Footer</h1>
-    </div>
+    <footer className=" flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
+      <div className=" flex items-center gap-4">
+        <img src={assets.logo} alt="" className=" hidden md:block w-20" />
+        <div className=" hidden md:block h-7 w-px bg-gray-500/60"></div>
+        <p>Copyright © 2025 LMS. All Right Reserved.</p>
+      </div>
+      <div className=" flex items-center gap-3 max-md:mt-4">
+        <a href="#">
+          <img src={assets.facebook_icon} alt="" />
+        </a>
+        <a href="#">
+          <img src={assets.twitter_icon} alt="" />
+        </a>
+        <a href="#">
+          <img src={assets.instagram_icon} alt="" />
+        </a>
+      </div>
+    </footer>
   );
 };
 
