5746efa3d408decd9bccf650675f2bb1e360544b
PranavTadvi <p.d.tadvi8825@gmail.com>
2025-03-04 16:08:54 +0530

    Created backend models , routes, controllers

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,10 +2,15 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
+import { useAuth, useUser } from "@clerk/clerk-react";
+
 export const AppContext = createContext();
 export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
   const navigate = useNavigate();
+
+  const { getToken } = useAuth();
+  const { user } = useUser();
   const [allCourses, setAllCourses] = useState([]);
   const [isEducator, setIsEducator] = useState(true);
   const [enrolledCourses, setEnrolledCourses] = useState([]);
@@ -34,6 +39,14 @@ export const AppContextProvider = (props) => {
     fetchAllCourses();
   }, []);
 
+  const logToken = async () => {
+    console.log(await getToken());
+  };
+  useEffect(() => {
+    if (user) {
+      logToken();
+    }
+  }, [user]);
   //Function to calculate Course Chapter TIme
   const calculateChapterTime = (chapter) => {
     let time = 0;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./configs/cloudinary.js";
 //initialize Expresss
 
 const app = express();
@@ -10,17 +13,18 @@ const app = express();
 //Connect to database
 
 await connectDB();
+await connectCloudinary();
 
 //Middleware
 app.use(cors());
+app.use(clerkMiddleware());
 
 //Routes
-
 app.get("/", (req, res) => res.send("API working"));
 app.post("/clerk", express.json(), clerkWebhooks);
+app.use("/api/v1/educator", express.json(), educatorRouter);
 
 //Port
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
